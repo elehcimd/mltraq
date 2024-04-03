1d9ea23b6cc128537f94b1fcc18b3f7cd262524e
Michele Dallachiesa <michele.dallachiesa@sigforge.com>
2024-04-03 11:46:44 +0200

    [tests] added more tests

---
+++ tests/test_job.py
@@ -0,0 +1,40 @@
+from mltraq.job import ProgressParallel
+import joblib
+from functools import partial
+import time
+
+
+def test_parallel():
+    """
+    Test: We can parallelize the execution of N tasks, retaining their order.
+    """
+
+    def simple_task(x):
+        return 2 * x
+
+    tasks = [partial(simple_task, i) for i in range(10)]
+
+    p = ProgressParallel(n_jobs=2, total=len(tasks), prefer="processes")
+    rets = p(joblib.delayed(func)() for func in tasks)
+
+    assert rets == [0, 2, 4, 6, 8, 10, 12, 14, 16, 18]
+
+
+def test_parallel_generator():
+    """
+    Test: We can return a generator instead a list. By itself, not useful. However, this is work
+    in preparation for joblib 1.4, which will provide "generator_unordered", very useful to make
+    jobs fail faster.
+    """
+
+    def simple_task(x):
+        return 2 * x
+
+    tasks = [partial(simple_task, i) for i in range(10)]
+
+    p = ProgressParallel(n_jobs=2, total=len(tasks), prefer="processes", return_as="generator")
+    gen = p(joblib.delayed(func)() for func in tasks)
+
+    rets = [next(gen) for _ in range(10)]
+
+    assert rets == [0, 2, 4, 6, 8, 10, 12, 14, 16, 18]
