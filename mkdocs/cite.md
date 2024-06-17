a53ef6522a3b1c1482c8cbae192ccfb9545a52fc
Michele Dallachiesa <michele.dallachiesa@sigforge.com>
2024-06-17 10:20:41 +0200

    added a step to drop fields in runs (for lightweight storage) and version bump

--- mkdocs/cite.md
+++ mkdocs/cite.md
@@ -6,7 +6,7 @@ You are welcome to cite MLtraq in your research:
 @software{mltraq2024,
   author = {Michele Dallachiesa},
   license = {BSD-3},
-  title = {MLtraq: Track and Collaborate on AI Experiments.},
+  title = {MLtraq: Track and Collaborate on ML/AI Experiments.},
   url = {https://www.mltraq.com/},
   year = {2024}
 }
---
+++ src/mltraq/steps/drop_fields.py
@@ -0,0 +1,20 @@
+from functools import partial
+
+from mltraq import Run
+
+
+def step_drop_fields(run: Run, fields=None):
+    """
+    Drop fields in the run.
+    """
+
+    if fields:
+        for name in fields:
+            del run.fields[name]
+
+
+def drop_fields(*fields) -> callable:
+    """
+    It returns a callable step that drops a set of fields.
+    """
+    return partial(step_drop_fields, fields=fields)
